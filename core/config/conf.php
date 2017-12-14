a02f71904065f51b77887b83f0c4854e516d53ab
moving7 <1115123081@qq.com>
2017-12-14 17:03:39 +0800

    修复URL模式 PATHINFO http://xxx.com/?a=b 报错问题

--- core/config/conf.php
+++ core/config/conf.php
@@ -14,5 +14,7 @@ return [
     /*控制计划任务*/
     'PLAN_START' => 1,
     /*预先执行时间 单位s*/
-    'BEFORE_TIME' => 0
+    'BEFORE_TIME' => 0,
+    /*是否开启pathinfo模式*/
+    "IS_PATHINFO" => 1,
 ];
--- core/lib/route.php
+++ core/lib/route.php
@@ -20,6 +20,17 @@ class route
 
         if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != '/') {
             $path = $_SERVER['REQUEST_URI'];
+            /*清除?之后的内容*/
+            $position = strpos($path, '?');
+            $path = $position === false ? $path : substr($path, 0, $position);
+            /*删除前后的“/”*/
+            $path = trim($path, '/');
+            /*修复xxx.com/?a=b 报错问题*/
+            if (!$path) {
+                $path = conf::get_conf('DEFAULT_CONTROLLER', 'route');
+            }
+            /*url处理*/
+//            $arr = parse_url($_SERVER['REQUEST_URI']);
             // 参数切割处理
             $path_arr = explode(DS, trim($path, DS));
             /*校验第一个参数*/
@@ -36,19 +47,51 @@ class route
             } else {
                 $this->action = conf::get_conf('DEFAULT_ACTION', 'route');
             }
+
             /*把URL多余的部分转换成GET的参数进行处理*/
-            $count_arr = count($path_arr) + 2;
-            $i = 2;
-            while ($i < $count_arr) {
-                /*当参数uri为奇数个时处理*/
-                if (isset($path_arr[$i + 1])) {
-                    $_GET[$path_arr[$i]] = $path_arr[$i + 1];
+            if ($path_arr) {
+                if (conf::get_conf('IS_PATHINFO', 'conf')) {
+                    $count_arr = count($path_arr) + 2;
+                    $i = 2;
+                    while ($i < $count_arr) {
+                        /*当参数uri为奇数个时处理*/
+                        if (isset($path_arr[$i + 1])) {
+                            $_GET[$path_arr[$i]] = $path_arr[$i + 1];
+                        }
+                        $i = $i + 2;
+                    }
+                } else {
+//                    $arr['query'] = isset($arr['query']) ? $arr['query'] : '';
+                    /*$arr['query'] = $arr['query'] ?? '';
+                    if($arr['query']) {
+                        $this->convertUrlQuery($arr['query']);
+                    }*/
                 }
-                $i = $i + 2;
             }
         } else {
             $this->controller = conf::get_conf('DEFAULT_CONTROLLER', 'route');
             $this->action = conf::get_conf('DEFAULT_ACTION', 'route');
         }
+    }
+
+    public function convertUrlQuery($query)
+    {
+        $queryParts = explode('&', $query);
+        $params = array();
+        foreach ($queryParts as $param) {
+            $item = explode('=', $param);
+            $params[$item[0]] = $item[1];
+        }
+        return $params;
+    }
+
+    public function getUrlQuery($array_query)
+    {
+        $tmp = array();
+        foreach ($array_query as $k => $param) {
+            $tmp[] = $k . '=' . $param;
+        }
+        $params = implode('&', $tmp);
+        return $params;
     }
 }
