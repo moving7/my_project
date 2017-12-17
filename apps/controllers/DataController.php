d9c4fcc5c22813e9d4cb451b34dbe60ecd7f7a0f
moving7 <1115123081@qq.com>
2017-12-17 18:07:14 +0800

    完善防止SQL注入 (PDO预处理+防非法参数过滤)

--- apps/controllers/DataController.php
+++ apps/controllers/DataController.php
@@ -26,12 +26,15 @@ class DataController extends wei
     /*添加日程页面*/
     public function add()
     {
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Pragma: no-cache");
         $this->display('index/add');
     }
 
     /*添加数据*/
     public function submit_data()
     {
+        /*防SQL注入参数过滤*/
         $res = Input('post.', null, 'htmlspecialchars');
         $res['day_type'] = checkParam($res['day_type']);
         $res['start_time'] = checkParam($res['start_time']);
@@ -48,7 +51,7 @@ class DataController extends wei
         } else {
             $is_remind = 0;
         }
-        $res = (new DataModel_w())->add(['u_id' => 1, 'start_time' => $res['start_time'], 'end_time' => $res['end_time'], 'event_info' => $res['event_info'], 'add_time' => time(), 'is_show' => 1, 'ahead_of_time' => $res['ahead'], 'is_remind' => $is_remind]);
+        $res = (new DataModel_w())->add_data(['u_id' => 1, 'start_time' => $res['start_time'], 'end_time' => $res['end_time'], 'event_info' => $res['event_info'], 'add_time' => time(), 'is_show' => 1, 'ahead_of_time' => $res['ahead'], 'is_remind' => $is_remind]);
         if (!$res) {
 //            echo json_encode(['code'=>404,'message'=>'Error']);
             echo "<script>alert('Error');location.href='/data/add'</script>";
--- apps/models/DataModel_w.php
+++ apps/models/DataModel_w.php
@@ -10,4 +10,20 @@ use core\lib\w_Model;
 class DataModel_w extends w_Model
 {
     public $_table = '115_dateinfo';
+
+    public function add_data($arr)
+    {
+        $sql = "INSERT INTO `115_dateinfo_copy` (u_id,start_time,end_time,event_info,add_time,is_show,ahead_of_time,is_remind) VALUES (:u_id,:start_time,:end_time,:event_info,:add_time,:is_show,:ahead_of_time,:is_remind)";
+        $sth = $this->_dbHandle->prepare($sql);
+        $sth->bindParam(':u_id', $arr['u_id']);
+        $sth->bindParam(':start_time', $arr['start_time']);
+        $sth->bindParam(':end_time', $arr['end_time']);
+        $sth->bindParam(':event_info', $arr['event_info']);
+        $sth->bindParam(':add_time', $arr['add_time']);
+        $sth->bindParam(':is_show', $arr['is_show']);
+        $sth->bindParam(':ahead_of_time', $arr['ahead_of_time']);
+        $sth->bindParam(':is_remind', $arr['is_remind']);
+        $result = $sth->execute();
+        return $result;
+    }
 }
